edfca4c5e0042804c6a19cdb16c79b8301937a0c
creos92 <33228553+creos92@users.noreply.github.com>
2018-03-09 18:38:36 +0100

    Update start_s.sh

--- experiments/start_s.sh
+++ experiments/start_s.sh
@@ -2,6 +2,7 @@
 source ./devel/setup.bash
 chmod +rx /Server/certificati/server/start_server.sh &&
 cd /Server/certificati/server && sync && ./start_server.sh &
+roscore &
 sleep 1s
 export ROS_IP=10.8.0.1
 export ROS_MASTER_URI=http://10.8.0.1:11311
