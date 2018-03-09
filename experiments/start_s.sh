0049aa74f5fdad71b4310c98aae59e55a2356092
creos92 <33228553+creos92@users.noreply.github.com>
2018-03-09 18:45:50 +0100

    Update start_s.sh

--- experiments/start_s.sh
+++ experiments/start_s.sh
@@ -2,9 +2,8 @@
 source ./devel/setup.bash
 chmod +rx /Server/certificati/server/start_server.sh &&
 cd /Server/certificati/server && sync && ./start_server.sh &
-roscore &
 sleep 1s
+roscore &
 export ROS_IP=10.8.0.1
 export ROS_MASTER_URI=http://10.8.0.1:11311
-cd /experiments && python sub.py
-
+cd /experiments && python sub.py &
