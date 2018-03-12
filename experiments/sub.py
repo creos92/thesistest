124116360235b61460dddb7c873e8c54179bc8f5
creos92 <33228553+creos92@users.noreply.github.com>
2018-03-12 15:14:49 +0100

    Update sub.py

--- experiments/sub.py
+++ experiments/sub.py
@@ -13,14 +13,17 @@ def callback(data):
     msg.append(str(len(data.data)))
     msg.append("{}.{}".format(data.ts.secs, data.ts.nsecs))
     msg.append("{}.{}".format(now.secs, now.nsecs))
-    f.write(", " .join(msg)+"\n")
+    #print data.data
+    if (data.data != "0"):
+        f.write(", " .join(msg)+"\n")
+    else:
+        print ("Dati caricati su data.dat")
+        f.close()
+        rospy.signal_shutdown("finish")
+
     #f.write(", ".join(msg) + "\n")
 
 def listener():
     rospy.init_node('listener', anonymous=True)
     rospy.Subscriber("chatter", Datats, callback)
     rospy.spin()
-
-if __name__ == '__main__':
-    listener()
-    f.close()
