5630c7e51d33bf36e1834ed3a74520c8ea2afc43
creos92 <33228553+creos92@users.noreply.github.com>
2018-03-13 20:31:55 +0100

    Update pub.py

--- experiments/pub.py
+++ experiments/pub.py
@@ -7,16 +7,19 @@ from speedtest.msg import Datats
 from std_msgs.msg import String
 
 def talker():
-    pub = rospy.Publisher('chatter', Datats, queue_size=5000)
+    pub = rospy.Publisher('chatter', Datats, queue_size=5500)
     rospy.init_node('talker', anonymous=True)
+    #rate = rospy.Rate(2000) # 10hz
     msg=Datats()
     print "invio dati"
+    time.sleep(2)
     for i in range(0,501):
         msg.data= ''.join(['.' for x in range(i)])
         for y in range(0,11):
-            msg.ts = rospy.get_rostime()
+            msg.ts = rospy.Time.now()
+            #print msg.ts
             pub.publish(msg)
-        rospy.sleep(0.05)
+        #rate.sleep()
     msg.data="0"
     print "termine invio dati\n"
     pub.publish(msg)
