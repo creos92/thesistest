2aad4554ba231ab606b0d131030b45c45ed49acc
creos92 <33228553+creos92@users.noreply.github.com>
2018-03-13 15:57:51 +0100

    Update pub.py

--- experiments/pub.py
+++ experiments/pub.py
@@ -9,22 +9,21 @@ from std_msgs.msg import String
 def talker():
     pub = rospy.Publisher('chatter', Datats, queue_size=5000)
     rospy.init_node('talker', anonymous=True)
-    #rate = rospy.Rate(10) # 10hz
     msg=Datats()
-    for i in range(0,500):
+    print "invio dati"
+    for i in range(0,501):
         msg.data= ''.join(['.' for x in range(i)])
         for y in range(0,11):
             msg.ts = rospy.get_rostime()
             pub.publish(msg)
         rospy.sleep(0.05)
     msg.data="0"
-    print "pubblico uno 0\n"
+    print "termine invio dati\n"
     pub.publish(msg)
-    time.sleep(30)
+    time.sleep(5)
 
 if __name__ == '__main__':
     try:
         talker()
     except rospy.ROSInterruptException:
         pass
-
