e1e1e088b730da5bf81bc23a059296ef60d18bd5
creos92 <33228553+creos92@users.noreply.github.com>
2018-03-13 15:57:05 +0100

    Update sub.py

--- experiments/sub.py
+++ experiments/sub.py
@@ -5,7 +5,6 @@ import time
 import string
 from speedtest.msg import Datats
 from std_msgs.msg import String
-f=open('data.dat','w')
 
 def callback(data):
     msg=[]
@@ -13,17 +12,19 @@ def callback(data):
     msg.append(str(len(data.data)))
     msg.append("{}.{}".format(data.ts.secs, data.ts.nsecs))
     msg.append("{}.{}".format(now.secs, now.nsecs))
-    #print data.data
     if (data.data != "0"):
-        f.write(", " .join(msg)+"\n")
+        mem.append(str(", " .join(msg)+"\n"))
     else:
-        print ("Dati caricati su data.dat")
+	f=open('data.dat','w')
+	for dato in mem:
+		f.write(dato)
+	print ("Dati caricati su data.dat")
         f.close()
         rospy.signal_shutdown("finish")
 
-    #f.write(", ".join(msg) + "\n")
-
 def listener():
+    global mem
+    mem=[]
     rospy.init_node('listener', anonymous=True)
     rospy.Subscriber("chatter", Datats, callback)
     rospy.spin()
